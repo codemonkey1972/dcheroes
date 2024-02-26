9308cb71ad67c9bb74811c4c408f435ef12bbeef
codemonkey1972 <codemonkey1972@gmail.com>
2024-02-26 00:01:57 -0500

    testing columns

--- module/documents/actor.mjs
+++ module/documents/actor.mjs
@@ -50,6 +50,7 @@ export class DCHeroesActor extends Actor {
     // Loop through ability scores, and add their modifiers to our sheet output.
     for (let [key, ability] of Object.entries(systemData.abilities)) {
       // Calculate the modifier using d20 rules.
+      // TODO fix this
       ability.mod = Math.floor((ability.value - 10) / 2);
     }
   }
