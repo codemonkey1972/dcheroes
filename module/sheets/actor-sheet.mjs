6cbcbc436bf8d45e38006c913c656c2d7a43ecba
codemonkey1972 <codemonkey1972@gmail.com>
2024-02-26 12:58:09 -0500

    Testing

--- module/sheets/actor-sheet.mjs
+++ module/sheets/actor-sheet.mjs
@@ -166,10 +166,8 @@ export class DCHeroesActorSheet extends ActorSheet {
     html.on('click', '.item-create', this._onItemCreate.bind(this));
 
     // Add Power Item
-    html.on('click', '.power-create', (ev) => {
-      this._onItemCreate.bind(this);
-    });
-
+    html.on('click', '.power-create', this._onItemCreate.bind(this));
+    
     // Delete Inventory Item
     html.on('click', '.item-delete', (ev) => {
       const li = $(ev.currentTarget).parents('.item');
