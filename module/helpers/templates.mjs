8010ce6ed660267c5983d4d82e178e3de029b83d
codemonkey1972 <codemonkey1972@gmail.com>
2024-02-26 09:36:39 -0500

    Add powers tab

--- module/helpers/templates.mjs
+++ module/helpers/templates.mjs
@@ -7,6 +7,7 @@ export const preloadHandlebarsTemplates = async function () {
   return loadTemplates([
     // Actor partials.
     'systems/dcheroes/templates/actor/parts/actor-features.hbs',
+    'systems/dcheroes/templates/actor/parts/actor-powers.hbs',
     'systems/dcheroes/templates/actor/parts/actor-items.hbs',
     'systems/dcheroes/templates/actor/parts/actor-spells.hbs',
     'systems/dcheroes/templates/actor/parts/actor-effects.hbs',
