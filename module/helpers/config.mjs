dd66ecd5d16645901abfd192f5bb4320f5d9ecdb
codemonkey1972 <codemonkey1972@gmail.com>
2024-02-28 20:12:02 -0500

    Bug fix?

--- module/helpers/config.mjs
+++ module/helpers/config.mjs
@@ -3,30 +3,30 @@ export const DCHEROES = {};
 // TODO add constants here
 
 /**
- * The set of Ability Scores used within the system.
+ * The set of Attribute Scores used within the system.
  * @type {Object}
  */
 DCHEROES.attributes = {
-  dex: 'DCHEROES.Ability.Dex.long',
-  str: 'DCHEROES.Ability.Str.long',
-  body: 'DCHEROES.Ability.Body.long',
-  int: 'DCHEROES.Ability.Int.long',
-  will: 'DCHEROES.Ability.Will.long',
-  mind: 'DCHEROES.Ability.Mind.long',
-  infl: 'DCHEROES.Ability.Infl.long',
-  aura: 'DCHEROES.Ability.Aura.long',
-  spirit: 'DCHEROES.Ability.Spirit.long',
+  dex: 'DCHEROES.Attribute.Dex.long',
+  str: 'DCHEROES.Attribute.Str.long',
+  body: 'DCHEROES.Attribute.Body.long',
+  int: 'DCHEROES.Attribute.Int.long',
+  will: 'DCHEROES.Attribute.Will.long',
+  mind: 'DCHEROES.Attribute.Mind.long',
+  infl: 'DCHEROES.Attribute.Infl.long',
+  aura: 'DCHEROES.Attribute.Aura.long',
+  spirit: 'DCHEROES.Attribute.Spirit.long',
 };
 
-DCHEROES.abilityAbbreviations = {
-  dex: 'DCHEROES.Ability.Dex.abbr',
-  str: 'DCHEROES.Ability.Str.abbr',
-  body: 'DCHEROES.Ability.Body.abbr',
-  int: 'DCHEROES.Ability.Int.abbr',
-  will: 'DCHEROES.Ability.Will.abbr',
-  mind: 'DCHEROES.Ability.Mind.abbr',
-  infl: 'DCHEROES.Ability.Infl.abbr',
-  aura: 'DCHEROES.Ability.Aura.abbr',
-  spirit: 'DCHEROES.Ability.Spirit.abSbr',
+DCHEROES.attributeAbbreviations = {
+  dex: 'DCHEROES.Attribute.Dex.abbr',
+  str: 'DCHEROES.Attribute.Str.abbr',
+  body: 'DCHEROES.Attribute.Body.abbr',
+  int: 'DCHEROES.Attribute.Int.abbr',
+  will: 'DCHEROES.Attribute.Will.abbr',
+  mind: 'DCHEROES.Attribute.Mind.abbr',
+  infl: 'DCHEROES.Attribute.Infl.abbr',
+  aura: 'DCHEROES.Attribute.Aura.abbr',
+  spirit: 'DCHEROES.Attribute.Spirit.abSbr',
 };
 
