5b39292666a57227254d244cf3b12df8b63df051
codemonkey1972 <codemonkey1972@gmail.com>
2024-03-02 13:08:14 -0500

    Bug fix

--- module/sheets/actor-sheet.mjs
+++ module/sheets/actor-sheet.mjs
@@ -96,7 +96,7 @@ export class DCHeroesActorSheet extends ActorSheet {
     }
     // Lightning Reflexes gives +2
     // Water Freedom applies when submerged in water
-    context.system.initiativeBonus = initiativeBonus;
+    context.system.initiativeBonus.value = initiativeBonus;
 
   }
 
