dad10773b557c6ac09c58566a98e24a79f1e6c12
codemonkey1972 <codemonkey1972@gmail.com>
2024-03-06 13:34:39 -0500

    Action roll calculations

--- module/sheets/actor-sheet.mjs
+++ module/sheets/actor-sheet.mjs
@@ -362,22 +362,27 @@ export class DCHeroesActorSheet extends ActorSheet {
       // get range index for AV
       const av = dataset.value;
       const avIndex = this._getRangeIndex(av);
-      //console.error(avIndex+" - value is "+av+"; range is ["+CONFIG.tables.ranges[avIndex][0]+" - "+CONFIG.tables.ranges[avIndex][1]+"]");
+      console.error(avIndex+" - value is "+av+"; range is ["+CONFIG.tables.ranges[avIndex][0]+" - "+CONFIG.tables.ranges[avIndex][1]+"]");
 
       // get range index for AV
       const ovIndex = this._getRangeIndex(ov);
-      //console.error(ovIndex+" - value is "+ov+"; range is ["+CONFIG.tables.ranges[ovIndex][0]+" - "+CONFIG.tables.ranges[ovIndex][1]+"]");
+      console.error(ovIndex+" - value is "+ov+"; range is ["+CONFIG.tables.ranges[ovIndex][0]+" - "+CONFIG.tables.ranges[ovIndex][1]+"]");
  
       // consult action chart for difficulty
       const actionTable = CONFIG.tables.actionTable;
       const difficulty = actionTable[avIndex][ovIndex];
-      // console.error(difficulty);
+      console.error("Difficulty = " +difficulty);
 
       // determine whether happens
       let avRoll = new Roll(dataset.roll, this.actor.getRollData());
 
+      avRoll.evaluate().then((response) => {
+        console.error(response);
+      });
+
       const avRollResult = avRoll._total;
       const avRollSuccess = avRoll._total >= difficulty;
+
       console.error(avRollResult + " : " + avRollSuccess);
       // avRoll.toMessage({
       //   speaker: ChatMessage.getSpeaker({ actor: this.actor }),
