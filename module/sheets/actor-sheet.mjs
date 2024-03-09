1a367a5e033031a0b43314adcb2ca1f468e3f469
codemonkey1972 <codemonkey1972@gmail.com>
2024-03-09 12:51:36 -0500

    Cleanup

--- module/sheets/actor-sheet.mjs
+++ module/sheets/actor-sheet.mjs
@@ -92,13 +92,15 @@ export class DCHeroesActorSheet extends ActorSheet {
       v.label = game.i18n.localize(CONFIG.DCHEROES.attributes[k]) ?? k;
     }
 
-    // The exception is player characters and sheets that have "link actor data" enabled (PCs do by default). For these actors there's a single character sheet shared by all copies of the actor.
+    // The exception is player characters and sheets that have "link actor data" enabled (PCs do by default). 
+    // For these actors there's a single character sheet shared by all copies of the actor.
+    // TODO If two Characters' Initiative totals are tied, a hero always takes precedence over a villain or minor Character.
     const initiativeBonus = this._calculateInitiativeBonus(context);
 
     // set value on actor sheet object
     // TODO do we need both of these now?
     context.system.initiativeBonus.value = initiativeBonus; // works for sheet display
-    context.actor.system.initiativeBonus.value = initiativeBonus; // already changes
+//    context.actor.system.initiativeBonus.value = initiativeBonus; // already changes
   }
 
   _calculateInitiativeBonus(context) {
