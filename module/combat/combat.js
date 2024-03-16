362de1a4359c152537dfd54730f440d80ee03378
codemonkey1972 <codemonkey1972@gmail.com>
2024-03-16 11:05:08 -0400

    Initiative

--- module/combat/combat.js
+++ module/combat/combat.js
@@ -1,2 +1,31 @@
+import { DCHEROES } from "../helpers/config.mjs";
+
 export default class MEGSCombat extends Combat { 
+
+    _sortCombatants(a, b) {
+        const initA = Number.isNumeric(a.initiative) ? a.initiative : -9999;
+        const initB = Number.isNumeric(b.initiative) ? b.initiative : -9999;
+        console.error("TYPES: "+initA+ " : "+initB);
+
+        const typeA = a.actor.data.type;
+        const typeB = b.actor.data.type;
+        console.error("TYPES: "+typeA+ " : "+typeB);
+
+        let initDifference = initB - initA;
+        if (initDifference != 0) {
+          return initDifference;
+        }
+    
+        if (typeA !== typeB) {
+          if (typeA === DCHEROES.characterTypes.hero) {
+            return -1;
+          }
+          if (typeB === DCHEROES.characterTypes.hero) {
+            return 1;
+          }
+        }
+    
+        return a.tokenId - b.tokenId;
+      }
+ 
 }
