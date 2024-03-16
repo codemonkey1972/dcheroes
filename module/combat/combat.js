5c17e5d5b34fb1e7fefb32ffa96a69eba7cd4cfa
codemonkey1972 <codemonkey1972@gmail.com>
2024-03-16 10:41:28 -0400

    Initiative

--- module/combat/combat.js
+++ module/combat/combat.js
@@ -5,7 +5,14 @@ export default class MEGSCombat extends Combat {
     _sortCombatants(a, b) {
         const initA = Number.isNumeric(a.initiative) ? a.initiative : -9999;
         const initB = Number.isNumeric(b.initiative) ? b.initiative : -9999;
-    
+        console.error("TYPES: "+typeA+ " : "+initB);
+
+        const typeA = a.actor.data.type;
+        const typeB = b.actor.data.type;
+        console.error("TYPES: "+initA+ " : "+typeB);
+
+        super._sortCombatants(a, b);
+/*    
         let initDifference = initB - initA;
         if (initDifference != 0) {
           return initDifference;
@@ -24,7 +31,7 @@ export default class MEGSCombat extends Combat {
           }
         }
     
-        return a.tokenId - b.tokenId;
+        return a.tokenId - b.tokenId;*/
       }
  
 }
