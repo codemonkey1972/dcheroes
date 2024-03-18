c23088e9b5910e9fd24327a3cdcaaa045d8e81e6
codemonkey1972 <codemonkey1972@gmail.com>
2024-03-18 14:09:30 -0400

    Combatant

--- module/combat/combatant.js
+++ module/combat/combatant.js
@@ -1,12 +1,16 @@
 export default class MEGSCombatant extends Combatant { 
     // https://foundryvtt.com/api/classes/client.Combatant.html
 
+    _onCreate(data, options, userID) {
+        super._onCreate(data, options, userID);
+        console.error("TEST: combatant._onCreate()")
+        this.actor.system.initiativeBonus  = this.actor._calculateInitiativeBonus();
+    }
+
     // getInitiativeRoll(formula: string): Roll
 
 
     _getInitiativeFormula(combatant) {
-        console.error("TEST: combatant.rollInitiative()")
-        this.actor.system.initiativeBonus  = this.actor._calculateInitiativeBonus();
         console.error(this.actor);
         return super._getInitiativeFormula(combatant);
       }
