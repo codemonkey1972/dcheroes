dcdc4eb41c96d590ed584970b19df71ee4fba664
codemonkey1972 <codemonkey1972@gmail.com>
2024-03-18 23:01:48 -0400

    Spend HP on initiative

--- module/combat/combatant.js
+++ module/combat/combatant.js
@@ -16,35 +16,42 @@ export default class MEGSCombatant extends Combatant {
 
         if (this.actor.system.heroPoints.value > 0) {
             console.error(this.actor.system.heroPoints);
-            const template = "systems/dcheroes/templates/actor/dialogs/initiativeDialog.hbs";
-            const data = {
-                "maxHpToSpend": this.actor.system.heroPoints.value,
-            };
-            renderTemplate(template, data).then((dialogHtml) => {
-                const d = new Dialog({
-                    title: "Spend HP on Initiative?",
-                    content: dialogHtml,
-                    buttons: {
-                    button2: {
-                        label: "Close",
-                        callback: (html) => {},
-                    },
-                    button1: {
-                        label: "Submit",
-                        callback: (html) => {
-                            console.error("TEST123");
-                            console.error(html);
-                            // TODO
-                            // baseFormula += ` + ${initiativeBonus}`;
-
-                            return baseFormula;
-                        }
-                    }
-                    },
-                    default: "button1"
-                }).render(true);
+            _handleHPInitiativeDialog.then((response) => {
+                console.error(response);
+                return baseFormula;
             });
+        } else {
+            return baseFormula;
         }
-        return baseFormula;
+    }
+
+    async _handleHPInitiativeDialog() {
+        const template = "systems/dcheroes/templates/actor/dialogs/initiativeDialog.hbs";
+        const data = {
+            "maxHpToSpend": this.actor.system.heroPoints.value,
+        };
+        let dialogHtml = await renderTemplate(template, data);
+        const d = new Dialog({
+            title: "Spend HP on Initiative?",
+            content: dialogHtml,
+            buttons: {
+            button2: {
+                label: "Close",
+                callback: (html) => {},
+            },
+            button1: {
+                label: "Submit",
+                callback: (html) => {
+                    console.error("TEST123");
+                    console.error(html);
+                    // TODO
+                    // baseFormula += ` + ${initiativeBonus}`;
+
+                    return baseFormula;
+                }
+            }
+            },
+            default: "button1"
+        }).render(true);
     }
 }
