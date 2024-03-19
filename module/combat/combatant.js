50c6031a310679f8e4e6202d44ccba51489103fa
codemonkey1972 <codemonkey1972@gmail.com>
2024-03-18 23:42:07 -0400

    Debugging

--- module/combat/combatant.js
+++ module/combat/combatant.js
@@ -19,46 +19,46 @@ export default class MEGSCombatant extends Combatant {
             let hasReturned = false;
 
             console.error("TEST1");
-            while (!hasReturned) {
-                if (!hasCalled) {
-                    console.error("TEST2");
-                    const template = "systems/dcheroes/templates/actor/dialogs/initiativeDialog.hbs";
-                    const data = {
-                        "maxHpToSpend": this.actor.system.heroPoints.value,
-                    };
-                    let dialogHtml = renderTemplate(template, data);
-                    console.error("TEST3");
-                    console.log(dialogHtml);
-                    const d = new Dialog({
-                        title: "Spend HP on Initiative?",
-                        content: dialogHtml,
-                        buttons: {
-                            button2: {
-                                label: "Close",
-                                callback: (html) => {},
-                            },
-                            button1: {
-                                label: "Submit",
-                                callback: (html) => {
-                                    console.error("TEST4");
-                                    console.error(html);
-                                    const form = html[0].querySelector('form');
-                                    console.error(form);
-                                    const hpSpentInitiative = parseInt(form.hpSpentInitiative.value) || 0;
-                                    console.error(hpSpentInitiative);
-                                    baseFormula += ` + ${hpSpentInitiative}`;
-                                    console.error(baseFormula);
-                                    hasReturned = true;
-                                }
-                            }
+            const template = "systems/dcheroes/templates/actor/dialogs/initiativeDialog.hbs";
+            const data = {
+                "maxHpToSpend": this.actor.system.heroPoints.value,
+            };
+            let dialogHtml = renderTemplate(template, data);
+            console.error("TEST2");
+            console.log(dialogHtml);
+            const d = new Dialog({
+                title: "Spend HP on Initiative?",
+                content: dialogHtml,
+                buttons: {
+                    button2: {
+                        label: "Close",
+                        callback: (html) => {
+                            hasReturned = true;
                         },
-                        default: "button1"
-                    }).render(true);
-                    console.error("TEST5");
-                    hasCalled = true;
-                }
+                    },
+                    button1: {
+                        label: "Submit",
+                        callback: (html) => {
+                            console.error("TEST4");
+                            console.error(html);
+                            const form = html[0].querySelector('form');
+                            console.error(form);
+                            const hpSpentInitiative = parseInt(form.hpSpentInitiative.value) || 0;
+                            console.error(hpSpentInitiative);
+                            baseFormula += ` + ${hpSpentInitiative}`;
+                            console.error(baseFormula);
+                            hasReturned = true;
+                        }
+                    }
+                },
+                default: "button1"
+            }).render(true);
+            console.error("TEST5");
+            hasCalled = true;
+
+            while (!hasReturned) {
             }
-            console.error("TEST3");
+            console.error("TEST6");
             return baseFormula;
         } else {
             return baseFormula;
