ed66d91f41249847f0dfabcc8534777c5903c6ce
codemonkey1972 <codemonkey1972@gmail.com>
2024-03-18 22:49:55 -0400

    Debugging

--- module/combat/combatant.js
+++ module/combat/combatant.js
@@ -14,32 +14,36 @@ export default class MEGSCombatant extends Combatant {
           baseFormula += ` + ${initiativeBonus}`;
         }
 
-        console.error(this.actor);
-        const template = "systems/dcheroes/templates/actor/dialogs/initiativeDialog.hbs";
-        const data = {
-          "maxHpToSpend": this.actor.system.heroPoints.value,
-        };
-        renderTemplate(template, data).then((dialogHtml) => {
-            const d = new Dialog({
-                title: "Spend HP on Initiative?",
-                content: dialogHtml,
-                buttons: {
-                  button2: {
-                    label: "Close",
-                    callback: (html) => {},
-                  },
-                  button1: {
-                    label: "Submit",
-                    callback: (html) => {
-                        console.error("TEST123");
-                        console.error(html);
-                    }
-                  }
-                },
-                default: "button1"
-              }).render(true);
-        });
+        if (this.actor.system.heroPoints > 0) {
+            const template = "systems/dcheroes/templates/actor/dialogs/initiativeDialog.hbs";
+            const data = {
+                "maxHpToSpend": this.actor.system.heroPoints.value,
+            };
+            renderTemplate(template, data).then((dialogHtml) => {
+                const d = new Dialog({
+                    title: "Spend HP on Initiative?",
+                    content: dialogHtml,
+                    buttons: {
+                    button2: {
+                        label: "Close",
+                        callback: (html) => {},
+                    },
+                    button1: {
+                        label: "Submit",
+                        callback: (html) => {
+                            console.error("TEST123");
+                            console.error(html);
+                            // TODO
+                            // baseFormula += ` + ${initiativeBonus}`;
 
+                            return baseFormula;
+                        }
+                    }
+                    },
+                    default: "button1"
+                }).render(true);
+            });
+        }
         return baseFormula;
     }
 }
