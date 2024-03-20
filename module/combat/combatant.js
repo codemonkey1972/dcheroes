b7420f6cd85e7836d95fe23c294e0ce5499c5cac
codemonkey1972 <codemonkey1972@gmail.com>
2024-03-20 15:40:24 -0400

    Debugging

--- module/combat/combatant.js
+++ module/combat/combatant.js
@@ -49,7 +49,7 @@ export default class MEGSCombatant extends Combatant {
     // }
     async _handleHPInitiativeDialog() {
         console.error("TEST3");
-        const template = "systems/dcheroes/templates/actor/dialogs/initiativeDialog.hbs";
+/*        const template = "systems/dcheroes/templates/actor/dialogs/initiativeDialog.hbs";
         const data = {
             // TODO
             // "maxHpToSpend": this.actor.system.heroPoints.value,
@@ -77,6 +77,44 @@ export default class MEGSCombatant extends Combatant {
             },
             default: "button1"
         }).render(true);
+*/
+        const template = "systems/dcheroes/templates/actor/dialogs/initiativeDialog.hbs";
+        const maxHpToSpend = 999;
+        const data = {
+            "maxHpToSpend": maxHpToSpend,
+        };
+        let dialogHtml = await this._renderTemplate(template, data);
 
+        new Dialog({
+            title: label,
+            content: dialogHtml,
+            buttons: {
+              button2: {
+                label: "Close",
+                callback: () => {},
+              },
+              button1: {
+                label: "Submit",
+                callback: (html) => {
+                    console.error("TEST4");
+                    const form = html[0].querySelector('form');
+                    const hpSpentInitiative = parseInt(form.hpSpentInitiative.value) || 0;
+                    return hpSpentInitiative
+                }
+              }
+            },
+            default: "button1"
+          }).render(true);
+    
+    }
+
+    /**
+     * 
+     * @param {*} template 
+     * @param {*} data 
+     * @returns 
+     */
+    async _renderTemplate(template, data) {
+        return await renderTemplate(template, data);
     }
 }
