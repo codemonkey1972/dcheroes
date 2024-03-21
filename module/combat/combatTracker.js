799ad720523c020b69316d7ce3c84362e270230e
codemonkey1972 <codemonkey1972@gmail.com>
2024-03-20 23:25:26 -0400

    Debugging

--- module/combat/combatTracker.js
+++ module/combat/combatTracker.js
@@ -2,6 +2,8 @@ export default class MEGSCombatTracker extends CombatTracker {
     async _onCombatantControl (event) {
         console.error("TEST9");
         console.error(event);
-        return super._onCombatantControl(event);
+        const test = super._onCombatantControl(event);
+        console.error(test);
+        return test;
     }
 }
